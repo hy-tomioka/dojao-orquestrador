446fd80417ff7f343e563fe0f31b52f56336f337
Alexandre <alexandre.marcos@zup.com.br>
2021-10-05 15:27:46 -0300

    feat: Primeiro commit

---
+++ src/main/java/br/com/zup/dojaoorquestrator/DojaoOrquestratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.dojaoorquestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojaoOrquestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojaoOrquestratorApplication.class, args);
+	}
+
+}
